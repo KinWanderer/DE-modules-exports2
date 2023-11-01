f6d7c5bf4931eda7791b9836ce96477b1db3f7bc
DCrawley94 <danikacrawley.dev@gmail.com>
2023-11-01 11:23:24 +0000

    add warmup task

---
+++ examples/file_5.py
@@ -0,0 +1,6 @@
+def print_different_greeting():
+    print('How are you doing?')
+
+
+def add(a, b):
+    return a + b
---
+++ index.py
@@ -0,0 +1,19 @@
+from examples.file_1 import mentor1
+from examples.file_2 import mentor2
+from examples.file_3 import number_list
+from examples.file_4 import print_greeting
+import examples.file_5 as something
+
+# Before you un-comment these print statements think about what you might see.
+
+# print(mentor1)
+
+# print(mentor2)
+
+# print(number_list)
+
+# print(print_greeting)
+# print(print_greeting())
+
+# print(something)
+# How might we use the variable `something` to invoke the functions imported from `file_5.py`?
