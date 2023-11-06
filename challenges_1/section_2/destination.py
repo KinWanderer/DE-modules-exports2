69759943268b47b3ad74dccaa0c94b503a1699b4
DCrawley94 <danikacrawley.dev@gmail.com>
2023-11-06 12:28:43 +0000

    add section 3 challenges

---
+++ challenges_1/section_2/destination.py
@@ -0,0 +1,3 @@
+#  Using the data stored in the nested source.py you should print the mentor's
+#  name to your terminal
+#  !!! Make sure you print only the * name *, not the whole data structure
---
+++ challenges_1/section_4/destination.py
@@ -0,0 +1,5 @@
+# Import the function from the utils.py file
+# Import the numbers from the nested data files
+# Invoke the function with those numbers and print the output to your terminal
+
+# You should see the number 35
